997daf6eeabfe84614f05ce95cd8b91f9ffca06f
Glendy <glendytuyuc@gmail.com>
2023-11-21 19:14:56 -0600

    enviar el pedido

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, createContext } from "react";
 import axios from "axios"
 import {toast} from "react-toastify"
+import { useRouter } from "next/router";
 
 const QuioscoContext = createContext()
 
@@ -10,6 +11,10 @@ const QuioscoProvider = ({children}) => {
     const [producto, setProducto] = useState({})
     const [modal, setModal] = useState(false)
     const [pedido, setPedido] = useState([])
+    const [nombre, setNombre] = useState("")
+    const [total, setTotal] = useState(0)
+
+    const router = useRouter();
 
     const obtenerCategorias = async () => {
         const {data} = await axios('/api/categorias')
@@ -26,7 +31,13 @@ const QuioscoProvider = ({children}) => {
     const handleClickCategoria = id => {
         const categoria = categorias.filter( cat => cat.id === id)
         setCategoriaActual(categoria[0])
+        router.push("/")
     }
+
+    useEffect(() => {
+        const nuevoTotal = pedido.reduce((total, producto) => (producto.precio * producto.cantidad) + total, 0)
+        setTotal(nuevoTotal)
+    }, [pedido])
 
     const handleSetProducto = producto => {
         setProducto(producto)
@@ -52,6 +63,44 @@ const QuioscoProvider = ({children}) => {
         setModal(false)
     }
 
+    const handleEditarCantidades = id => {
+        const productoActualizar = pedido.filter( producto => producto.id === id)
+        setProducto(productoActualizar[0])
+
+        setModal(!modal)
+    }
+
+    const handleEliminarProducto = id => {
+        const pedidoActualizado = pedido.filter(producto => producto.id !== id)
+        setPedido(pedidoActualizado)
+
+    }
+
+    const colocarOrden = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axios.post('/api/ordenes', {pedido,nombre, total,fecha: Date.now().toString()} )
+
+            //Resetear la app
+            setCategoriaActual(categorias[0])
+            setPedido([])
+            setNombre("")
+            setTotal(0)
+
+            toast.success("Pedido Realizado Correctamente")
+
+            setTimeout(() => {
+                router.push("/")
+            }, 2000)
+
+        } catch (error) {
+            console.log(error)
+        }
+      
+   }
+
+
     return (
         <QuioscoContext.Provider
         value={{
@@ -63,7 +112,13 @@ const QuioscoProvider = ({children}) => {
             modal,
             handleChangeModal,
             handleAgregarPedido,
-            pedido
+            pedido,
+            handleEditarCantidades,
+            handleEliminarProducto,
+            nombre,
+            setNombre,
+            colocarOrden,
+            total   
         }}>
             {children}
 
